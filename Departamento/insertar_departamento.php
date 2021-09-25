39e644a764f43d2c3ec28497bcf4da2bead4daea
Diego Sicaja Morales <armando.sicaja.morales@ericsson.com>
2021-09-24 18:13:37 -0600

    Departamento

---
+++ Departamento/insertar_departamento.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+     <title>
+        Aplicacion de Ejemplo
+     </title>
+  </head>
+  <body>
+
+
+<?php
+
+$codigo=$_GET["codigo"];
+$nombre=$_GET["nombre"];
+
+$link = mysqli_connect('localhost', 'root', 'root','basesproyectofinal') or die('Could not connect: ' . mysqli_error());
+
+$query = "INSERT INTO Departamento VALUES ($codigo,'$nombre')";
+
+$result = mysqli_query($link,$query) or die('Hubo un error: ' . mysqli_error($link));
+echo 'el registro fue insertado exitosamente<br>';
+
+mysqli_close($link);
+
+
+?>
+
+
+     <center>
+         <a href="index.html">regresar</a>
+     </center>
+  </body>
+</html>
--- Empleado/insertar_empleado.php
+++ Empleado/insertar_empleado.php
@@ -14,7 +14,7 @@ $nombre=$_GET["nombre"];
 
 $link = mysqli_connect('localhost', 'root', 'root','basesproyectofinal') or die('Could not connect: ' . mysqli_error());
 
-$query = "INSERT INTO Empleado VALUES ($codigo,'$nombre')";
+$query = "INSERT INTO Empleado VALUES ($codigo,'$nombre',1)";
 
 $result = mysqli_query($link,$query) or die('Hubo un error: ' . mysqli_error($link));
 echo 'el registro fue insertado exitosamente<br>';
