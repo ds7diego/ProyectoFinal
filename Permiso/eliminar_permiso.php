e2db0f5c8b0316d6ebb374ba959bf31a2dd3fe47
Diego Sicaja Morales <armando.sicaja.morales@ericsson.com>
2021-09-24 21:38:44 -0600

    permiso

---
+++ Permiso/eliminar_permiso.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+     <title>
+        Eliminar departamento
+     </title>
+  </head>
+  <body>
+
+
+<?php
+
+$codigo=$_GET["codigo"];
+
+$link = mysqli_connect('localhost', 'root', '','basesproyectofinal') or die('Could not connect: ' . mysqli_error());
+
+$query = "DELETE FROM Departamento WHERE Codigo_Departamento=$codigo";
+
+$result = mysqli_query($link,$query) or die('Query failed: ' . mysqli_error($link));
+
+echo 'el registro fue eliminado exitosamente<br>';
+
+mysqli_close($link);
+
+
+?>
+
+
+     <center>
+         <a href="../index.html">regresar</a>
+     </center>
+  </body>
+</html>
---
+++ Permiso/insertar_permiso.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+     <title>
+        Insertar permiso
+     </title>
+  </head>
+  <body>
+
+
+<?php
+
+$codigo=$_GET["codigo"];
+$nombre=$_GET["nombre"];
+
+$link = mysqli_connect('localhost', 'root', '','basesproyectofinal') or die('Could not connect: ' . mysqli_error());
+
+$query = "INSERT INTO Departamento VALUES ($codigo,'$nombre')";
+
+$result = mysqli_query($link,$query) or die('Hubo un error: ' . mysqli_error($link));
+echo 'el registro fue insertado exitosamente<br>';
+
+mysqli_close($link);
+
+
+?>
+
+
+     <center>
+         <a href="../index.html">regresar</a>
+     </center>
+  </body>
+</html>
