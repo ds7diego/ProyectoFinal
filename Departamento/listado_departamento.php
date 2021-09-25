53fc5a09a78e71d62bc5054ae4ec52d38095fa9c
Diego Sicaja Morales <armando.sicaja.morales@ericsson.com>
2021-09-24 18:17:27 -0600

    departamento

--- Departamento/listado_departamento.php
+++ Departamento/listado_departamento.php
@@ -32,8 +32,8 @@ while ($line = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
    echo "\t<tr>\n";
    echo "\t\t<td>$codigo</td>\n";
    echo "\t\t<td>$nombre</td>\n";
-   echo "\t\t<td><a href=eliminar.php?codigo=$codigo><img src=/imagenes/icon-remove.png></a></td>\n";
-   echo "\t\t<td><a href=modificar_forma.php?codigo=$codigo&nombre=$nombre><img src=/imagenes/icon-edit.jpg></a></td>\n";
+   echo "\t\t<td><a href=eliminar.php?codigo=$codigo><img src=icon-remove.png></a></td>\n";
+   echo "\t\t<td><a href=modificar_forma.php?codigo=$codigo&nombre=$nombre><img src=icon-edit.jpg></a></td>\n";
    echo "\t</tr>\n";
 }
 echo "</table>\n";
